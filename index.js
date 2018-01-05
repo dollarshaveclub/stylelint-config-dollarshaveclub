3dc7bbc61ad73b1254d48c55845fd2c34fcfa7c0
Jeff Wainwright <yowainwright@gmail.com>
2018-01-05 14:45:58 -0800

    [update] adds working tests for rules

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 "Use Strict";
 
 module.exports = {
-  extends: "stylelint-config-standard",
-  "rules": {
-    "color-hex-long": "long",
-    "color-no-hex": true,
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "color-hex-length": "long",
   },
-};
+}
--- tests/index.js
+++ tests/index.js
@@ -3,40 +3,36 @@
   ---
 
 */
-const fs = require("fs");
-const exec = require("child_process").exec;
-const path = require("path");
-const globby = require("globby");
-const assert = require("assert");
+const fs = require("fs")
+const exec = require("child_process").exec
+const path = require("path")
+const globby = require("globby")
+const assert = require("assert")
 
-const passingFiles = globby.sync(["tests/fixtures/**/pass/*.css"]);
-const failingFiles = globby.sync(["tests/fixtures/**/fail/*.css"]);
-
-const TSLINT_CMD = "npx stylelint -c index.js";
+const passingFiles = globby.sync(["tests/fixtures/**/pass/*.css"])
+const failingFiles = globby.sync(["tests/fixtures/**/fail/*.css"])
 
 passingFiles.forEach(passingFilename => {
   it(`${passingFilename} should pass linting`, done => {
-    exec(`${TSLINT_CMD} ${passingFilename}`, (err, stdout, stderr) => {
+    exec(`npx stylelint "${passingFilename}" --config index.js`, (err, stdout, stderr) => {
       if (err) {
         console.error(err.stack);
-        console.error(stdout.toString());
-        console.error(stderr.toString());
-        done(err);
-        return;
+        console.error(stdout.toString())
+        console.error(stderr.toString())
+        done(err)
+        return
       }
-
-      done();
-    });
-  });
-});
+      done()
+    })
+  })
+})
 
 failingFiles.forEach(failingFilename => {
   it(`${failingFilename} should not pass linting`, done => {
-    exec(`${TSLINT_CMD} ${failingFilename}`, (err, stdout, stderr) => {
-      /* eslint no-console: 0 */
+    exec(`npx stylelint "${failingFilename} --config index.js`, (err, stdout, stderr) => {
       assert(err);
-      console.log(stdout);
-      done();
-    });
-  });
-});
+      console.log(stdout)
+      done()
+    })
+  })
+})
