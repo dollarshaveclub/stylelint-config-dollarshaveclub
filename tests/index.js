944612db624238d2f4087ef8946b24ce1ec2a811
Jeff Wainwright <yowainwright@gmail.com>
2018-01-10 13:57:55 -0800

    [version] version bump

--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,19 @@
-/*
-  tests
-  ---
-
-*/
 const exec = require("child_process").exec
 const globby = require("globby")
 const assert = require("assert")
 
-const passingFiles = globby.sync(["tests/fixtures/**/pass/*.css"])
-const failingFiles = globby.sync(["tests/fixtures/**/fail/*.css"])
+// css globs
+const passingCSSFiles = globby.sync(["tests/fixtures/**/pass/*.css"])
+const failingCSSFiles = globby.sync(["tests/fixtures/**/fail/*.css"])
 
-passingFiles.forEach(passingFilename => {
-  it(`${passingFilename} should pass linting`, done => {
-    exec(`npx stylelint "${passingFilename}" --config index.js`, (err, stdout, stderr) => {
+// scss globs
+const passingSCSSFiles = globby.sync(["tests/fixtures/**/pass/*.scss"])
+const failingSCSSFiles = globby.sync(["tests/fixtures/**/fail/*.scss"])
+
+// css tests
+passingCSSFiles.forEach(passingCSSFilename => {
+  it(`${passingCSSFilename} should pass linting`, done => {
+    exec(`npx stylelint "${passingCSSFilename}" --config index.js`, (err, stdout, stderr) => {
       if (err) {
         console.error(err.stack)
         console.error(stdout.toString())
@@ -25,9 +26,35 @@ passingFiles.forEach(passingFilename => {
   })
 })
 
-failingFiles.forEach(failingFilename => {
-  it(`${failingFilename} should not pass linting`, done => {
-    exec(`npx stylelint "${failingFilename} --config index.js`, (err, stdout, stderr) => {
+failingCSSFiles.forEach(failingCSSFilename => {
+  it(`${failingCSSFilename} should not pass linting`, done => {
+    exec(`npx stylelint "${failingCSSFilename} --config index.js`, (err, stdout, stderr) => {
+      assert(err)
+      console.log(stdout)
+      done()
+    })
+  })
+})
+
+// scss tests
+passingSCSSFiles.forEach(passingSCSSFilename => {
+  it(`${passingSCSSFilename} should pass linting`, done => {
+    exec(`npx stylelint "${passingSCSSFilename}" --config scss.js`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err.stack)
+        console.error(stdout.toString())
+        console.error(stderr.toString())
+        done(err)
+        return
+      }
+      done()
+    })
+  })
+})
+
+failingSCSSFiles.forEach(failingSCSSFilename => {
+  it(`${failingSCSSFilename} should not pass linting`, done => {
+    exec(`npx stylelint "${failingSCSSFilename} --config scss.js`, (err, stdout, stderr) => {
       assert(err)
       console.log(stdout)
       done()
